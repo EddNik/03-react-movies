c844fa10dbd1932babb3f352ac584c6757fa6a88
Eduard <nikinna86@gmail.com>
2025-09-24 11:43:02 +0300

    update

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+// import { useState } from "react";
+
+import "./App.css";
+
+export default function App() {
+  const API_TOKEN = import.meta.env.VITE_TMDB_TOKEN;
+
+  console.log(API_TOKEN);
+  return <></>;
+}
